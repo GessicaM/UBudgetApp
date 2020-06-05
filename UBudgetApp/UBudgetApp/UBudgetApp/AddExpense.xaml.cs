1541be56cdb9283f61a3036bd1635d951b7b4059
Ahsan Farooq <ahsan.farooq@yahoo.com>
2020-06-05 03:35:26 -0700

    Saving Expenses to file such that they appear on reopening of app.

--- UBudgetApp/UBudgetApp/UBudgetApp/AddExpense.xaml.cs
+++ UBudgetApp/UBudgetApp/UBudgetApp/AddExpense.xaml.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,80 +17,97 @@ namespace UBudgetApp
         public AddExpense()
         {
             InitializeComponent();
-
-
         }
-        private void OnSaveButtonClicked(object sender, EventArgs e)
+        private async void OnSaveButtonClicked(object sender, EventArgs e)
         {
             Expense expense = ((AddExpenseViewModel)BindingContext).Expense;
-
-            if (expense.ExpenseId == 0)
+            if (string.IsNullOrWhiteSpace(expense.ExpenseFile))
             {
-                switch (expense.Category)
-                {
-                    case "Transport":
-                        expense.PictureUrl = "Assets/Icons/autoNTransport.png";
-                        break;
-                    case "Education":
-                        expense.PictureUrl = "Assets/Icons/education.png";
-                        break;
-                    case "Entertainment":
-                        expense.PictureUrl = "Assets/Icons/entertainment.png";
-                        break;
-                    case "Eating Out":
-                        expense.PictureUrl = "Assets/Icons/foodNDining.png";
-
-                        break;
-                    case "Gifts":
-                        expense.PictureUrl = "Assets/Icons/gifts.png";
-
-                        break;
-                    case "Health":
-                        expense.PictureUrl = "Assets/Icons/healthNFitness.png";
-                        break;
-                    case "Kids":
-                        expense.PictureUrl = "Assets/Icons/kids.png";
-                        break;
-                    case "Miscellaneous":
-                        expense.PictureUrl = "Assets/Icons/miscellaneous.png";
-                        break;
-                    case "Personal Care":
-                        expense.PictureUrl = "Assets/Icons/personalCare.png";
-                        break;
-                    case "Shopping":
-                        expense.PictureUrl = "Assets/Icons/shopping.png";
-                        break;
-                    case "Travel":
-                        expense.PictureUrl = "Assets/Icons/travel.png";
-                        break;
-                    case "Utilities":
-                        expense.PictureUrl = "Assets/Icons/utilities.png";
-
-                        break;
-
-                }
+                //create and save expense
+                var filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                    $"{Path.GetRandomFileName()}.expense.txt");
+                File.AppendAllText(filename, $"{NameEntry.Text}\n");
+                File.AppendAllText(filename, $"{AmountEntry.Text}\n");
+                File.AppendAllText(filename, $"{DatePicker.Date}\n");
+                File.AppendAllText(filename, $"{CategoryPicker.SelectedItem}\n");
+                File.AppendAllText(filename, $"{IconAddress($"{CategoryPicker.SelectedItem}")}");
+            }
+            else
+            {
+                //update existing expense
+                File.WriteAllText(expense.ExpenseFile, $"{NameEntry.Text}\n");
+                File.AppendAllText(expense.ExpenseFile, $"{AmountEntry.Text}\n");
+                File.AppendAllText(expense.ExpenseFile, $"{DatePicker.Date}\n");
+                File.AppendAllText(expense.ExpenseFile, $"{CategoryPicker.SelectedItem}\n");
+                File.AppendAllText(expense.ExpenseFile, $"{IconAddress($"{CategoryPicker.SelectedItem}")}");
             }
 
             MessagingCenter.Send(this, "AddExpense", expense);
-
-            Navigation.PopModalAsync();
-
-
-
+            await Navigation.PopModalAsync();
         }
-
-        private void OnCancelButtonClicked(object sender, EventArgs e)
+        private async void OnDeleteButtonClicked(object sender, EventArgs e)
         {
-
-            //To go back to ExpenseListpage
-            Navigation.PopModalAsync();
-
+            var expense = (Expense)BindingContext;
+            if (File.Exists(expense.ExpenseFile))
+            {
+                File.Delete(expense.ExpenseFile);
+            }
+            //To go back to previous page
+            await Navigation.PopModalAsync();
         }
         private void SelectedDatePicker_DateSelected(object sender, DateChangedEventArgs e)
         {
             var NewDate = e.NewDate.ToShortDateString();
+        }
 
+        public static string IconAddress(string cat)
+        {
+            string IconAddress = "";
+            switch (cat)
+            {
+                case "Transport":
+                    IconAddress = "Assets/Icons/autoNTransport.png";
+                    break;
+                case "Education":
+                    IconAddress = "Assets/Icons/education.png";
+                    break;
+                case "Entertainment":
+                    IconAddress = "Assets/Icons/entertainment.png";
+                    break;
+                case "Eating Out":
+                    IconAddress = "Assets/Icons/foodNDining.png";
+                    break;
+                case "Gifts":
+                    IconAddress = "Assets/Icons/gifts.png";
+                    break;
+                case "Health":
+                    IconAddress = "Assets/Icons/healthNFitness.png";
+                    break;
+                case "Kids":
+                    IconAddress = "Assets/Icons/kids.png";
+                    break;
+                case "Miscellaneous":
+                    IconAddress = "Assets/Icons/miscellaneous.png";
+                    break;
+                case "Personal Care":
+                    IconAddress = "Assets/Icons/personalCare.png";
+                    break;
+                case "Shopping":
+                    IconAddress = "Assets/Icons/shopping.png";
+                    break;
+                case "Travel":
+                    IconAddress = "Assets/Icons/travel.png";
+                    break;
+                case "Utilities":
+                    IconAddress = "Assets/Icons/utilities.png";
+                    break;
+            }
+            return IconAddress;
+        }
 
+        private async void OnCancelButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
         }
     }
 }
--- UBudgetApp/UBudgetApp/UBudgetApp/Model/Expense.cs
+++ UBudgetApp/UBudgetApp/UBudgetApp/Model/Expense.cs
@@ -7,26 +7,23 @@ namespace UBudgetApp.Model
     public class Expense
     {
         public int ExpenseId { get; set; }
+        public string ExpenseFile { get; set; }
         public string ExpenseName { get; set; }
         public double Amount { get; set; }
-        public string Category { get; set; }
         public DateTime Date { get; set; }
-
+        public string Category { get; set; }
         public string PictureUrl { get; set; }
 
 
-        public Expense(int expenseId, string expenseName, double amount, DateTime date, string category, string pictureUrl)
-        {
-            ExpenseId = expenseId;
-            ExpenseName = expenseName;
-            Amount = amount;
-            Category = category;
-            Date = date;
-            PictureUrl = pictureUrl;
-
-
-
-        }
+        //public Expense(int expenseId, string expenseName, double amount, DateTime date, string category, string pictureUrl)
+        //{
+        //    ExpenseId = expenseId;
+        //    ExpenseName = expenseName;
+        //    Amount = amount;
+        //    Category = category;
+        //    Date = date;
+        //    PictureUrl = pictureUrl;
+        //}
 
         public Expense()
         {
